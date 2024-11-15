d0539b3e9513df0cf50b239156da21af9297d419
Ochudho <ochudho.c@gmail.com>
2024-11-15 18:04:39 +0200

    Convert markdown content to html

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -11,13 +11,12 @@ def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
-
-
+    
 def entry(request, title):
     content = util.get_entry(title)
     if content is None:
         return HttpResponse("Entry Not Found.")
-    html_content = markdown2.Markdown(content)
+    html_content = markdown2.markdown(content)
     return render(request, "encyclopedia/entry.html", {
         "title": title,
         "content": html_content
