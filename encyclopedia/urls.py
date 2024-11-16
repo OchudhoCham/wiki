4bd7c340d238fc79ef6e8beb9131178d190bc20d
Ochudho <ochudho.c@gmail.com>
2024-11-16 16:04:09 +0200

    Linked new_page & rando_page with layut.html

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("wiki/<str:title>", views.entry, name="entry"),
     path("wiki/search", views.search, name="search"),
-    path("wiki/new", views.new_page, name="new_page"),
+    path("new", views.new_page, name="new_page"),
     path("edit/<str:title>", views.edit_page, name="edit_page"),
     path("wiki/random", views.random_page, name="random_page")
 ]
